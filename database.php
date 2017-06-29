b3478747f14e5e2a68bf6550c1da32188381ebca
Tagrat <tagg12352@gmail.com>
2017-06-29 15:55:44 -0400

    final copy

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+    $dsn = 'mysql:host=sql.njit.edu;dbname=wt49';
+    $username = 'wt49';
+    $password = 'CImYkE0rQ';
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('database_error.php');
+        exit();
+    }
+?>
